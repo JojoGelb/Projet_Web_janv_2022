a13cc61792fd3bf16c99ff56ca7494534c13c2fd
Jordy Gelb <jordygelb@gmail.com>
2022-01-06 16:47:05 +0100

    Ajout d'un script de generation d'html dans la page d'acceuil

---
+++ script/acceuil.js
@@ -0,0 +1,27 @@
+var table = ["pain5","pain","pain2","pain3","pain4","pain5"];
+
+var table = [
+    {"nom":"pain5","text":"Elles sont belles mes miches","alt":"image_de_pain5"},
+    {"nom":"pain","text":"Tu veux mon pain?","alt":"image_de_pain"},
+    {"nom":"pain2","text":"Te fais pas rouler dans la farine","alt":"image_de_pain1"},
+    {"nom":"pain3","text":"On dit le pain pas lapin","alt":"image_de_pain2"},
+    {"nom":"pain4","text":"Sapin? non son pain","alt":"image_de_pain3"}
+];
+
+
+window.onload = function load_my_sliders() {
+    create_slide_show()
+}
+
+
+function create_slide_show(){
+    var result = "";
+    var start = "<figure><img src=\"./ressources/"
+    var mid = "\" width=\"1240\" height=\"620\" /><figcaption>"
+    for(var i =0; i< table.length; i++){
+        result += start + table[i].nom + ".jpg\" alt=\"" + table[i].alt + mid + table[i].text + "</figcaption></figure>"
+    }
+    result += start + table[0].nom + ".jpg\" alt=\"" + table[0].alt + mid + table[0].text + "</figcaption></figure>"
+    document.getElementById("slider").innerHTML += result;
+}
+
